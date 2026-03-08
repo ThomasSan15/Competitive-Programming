df45ccbe6519ff9cfc66264bf52e6d534afd2a70
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-08 14:22:22 -0500

    add template .gitignore

---
+++ template/template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using ll = long long;
+using ld = long double;
+
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+int main() {
+    fastio
+
+    int t = 1;
+    // cin >> t;   // usar si hay múltiples casos
+
+    while (t--) {
+       
+    }
+
+    return 0;
+}
