9d73b833ea56e52f152fd5c7ff717f8fe428e76b
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-13 10:52:55 -0500

    Add problem B contest_2207

---
+++ Codeforces/Contest_2207/ProblemB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t = 0, n = 0, m = 0, l = 0;
+    
+    cin >> t;
+
+    for(; t > 0 ; t--){
+        cin >> n >> m >> l;
+        
+    }
+}
