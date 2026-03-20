9dffaac4a99346b4b849486400432ea0d4c1212a
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-19 21:28:36 -0500

    add palindromes! problem

--- XXXIXMaratónNacionaldeProgramación_Colombia-ACIS/just.cpp
+++ XXXIXMaratónNacionaldeProgramación_Colombia-ACIS/just.cpp
@@ -6,14 +6,22 @@ using namespace std;
 int palindrome(string a){
 
     for(int i = 0, j = a.size() - 1; i != j;){
-        if(a.at(i) != ' ')
+        if(a.at(i) != ' '){
+          if(a.at(j) != ' '){
        if(a.at(i) == a.at(j) ){
          i++; 
          j--;
        }else{
         return 0;
        }
+      }
+      else{
+        j++;
+       }
+    }else{
+      i++;
     }
+  }
     return 1;
 }
 
@@ -22,7 +30,20 @@ for(int i = 0; i < a.size() - 1; i++){
        if(a.at(i) >= 65 && a.at(i) <= 90){
          a.at(i) += 32;
        }else{
-        a.erase(i);
+        if(a.at(i) < 97 || a.at(i) > 122 ) 
+        a.erase(1,i);
        }
     }
+
+    return a;
+}
+
+int main(){
+  string a;
+  getline(cin , a);
+  cout << a << endl;
+  a = reformed(a);
+   cout << a << endl;
+
+    palindrome(a) ?  cout << "Y" : cout << "N";
 }
