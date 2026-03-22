641f90df00a48ac1fce596303b4bdb03c5cea3be
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-22 12:43:14 -0500

    update problem F

--- Codeforces/Gym_106420/F.cpp
+++ Codeforces/Gym_106420/F.cpp
@@ -2,3 +2,21 @@
 #include <vector>
 
 using namespace std;
+
+int main(){
+    int n = 0, q = 0, d = 0, value = 0;
+    vector <int> a;
+    vector<int> s;
+    cin >> n >> q >> d;
+
+    for(int i = 0; i < n; i++){
+        cin >> value;
+        a.push_back(value);
+    }
+    for(int i = 0; i < n; i++){
+        cin >> value;
+        s.push_back(value);
+    }
+    
+
+}
