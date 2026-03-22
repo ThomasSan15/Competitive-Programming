2cb10490a2279d3daf0bf26024ecffdcbb9ada05
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-21 21:36:31 -0500

    update problem E

--- Codeforces/Gym_106420/E.cpp
+++ Codeforces/Gym_106420/E.cpp
@@ -1,1 +1,41 @@
+
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, h;
+    cin >> n >> h;
+    vector<int> vectorDuraciones(n);
+    vector<int> vectorDPS(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vectorDuraciones[i] >> vectorDPS[i];
+    }
+
+    int level = 0;
+    int temporalHP = h;
+    while (h > 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int duracion = vectorDuraciones[i];
+            int dps = vectorDPS[i];
+            int dañoTotal = duracion * (dps - level);
+            if (dañoTotal < 0)
+            {
+                dañoTotal = 0;
+            }
+            h -= dañoTotal;
+        }
+        if (h > 0)
+        {
+            break;
+        }
+        level++;
+        h = temporalHP;
+    }
+    cout << level << endl;
+}
