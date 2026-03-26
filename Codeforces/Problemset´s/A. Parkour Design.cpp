8f94bfb2774b816380007318f86c56f81813c6db
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-25 20:47:47 -0500

    add Problemset Folder

---
+++ Codeforces/Problemset´s/A. Parkour Design.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/2202/A
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t, x , y;
+    cin >> t;
+    while(t){
+        
+        t--;
+    }
+}
