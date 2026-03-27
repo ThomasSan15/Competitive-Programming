8e075977f1aa440a386551acbde92025dc01c8cb
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-26 22:28:43 -0500

    finish friendly numbers

--- Codeforces/Problemset´s/A. Friendly Numbers.cpp
+++ Codeforces/Problemset´s/A. Friendly Numbers.cpp
@@ -1,6 +1,7 @@
 //http://codeforces.com/problemset/problem/2197/A
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -8,16 +9,34 @@ int Sum_Digits(int x){
     int sum = 0;
     while(x > 0){
         sum += x % 10;
-        x /= x;
+        x = x / 10;
     }
+    return sum;
+}
+
+int Digits(int x){
+    int count = 0;
+     while(x > 0){
+        count++;
+        x = x / 10;
+    }
+     count = pow(10,count);
+    return count;
 }
 
 int main(){
-    int t, x ,y, count;
+    int t, x ,y, count ,aux;
     cin >> t;
     while(t--){
-        for(int i = 0; i < x; i++){
-            if(i)
+        cin >> x;
+        aux = Digits(x);
+        for(y = 0; y < aux; y++){
+            if(y - Sum_Digits(y) == x){
+                count++;
+            }
         }
+        cout << count << endl;
+        count = 0;
     }
+
 }
