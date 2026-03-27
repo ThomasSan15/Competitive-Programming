48f979aa8f26908b9bd37bb3d80b0462a15dbd10
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-26 22:08:38 -0500

    Friendly numbers

---
+++ Codeforces/Problemset´s/A. Friendly Numbers.cpp
@@ -0,0 +1,23 @@
+//http://codeforces.com/problemset/problem/2197/A
+
+#include <iostream>
+
+using namespace std;
+
+int Sum_Digits(int x){
+    int sum = 0;
+    while(x > 0){
+        sum += x % 10;
+        x /= x;
+    }
+}
+
+int main(){
+    int t, x ,y, count;
+    cin >> t;
+    while(t--){
+        for(int i = 0; i < x; i++){
+            if(i)
+        }
+    }
+}
