4676f5c99a42fdbce1ac2fae9a331784c551c66a
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-27 21:36:48 -0500

    add parkour desing problem

--- Codeforces/Problemset´s/A. Parkour Design.cpp
+++ Codeforces/Problemset´s/A. Parkour Design.cpp
@@ -1,19 +1,47 @@
-//https://codeforces.com/problemset/problem/2202/A
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t, x , y;
-    cin >> t;
-    while(t){
-        cin >> x >> y;
 
-        if((x + 1) >= 0 && y >= 0 ){
-            cout << "yes";
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using ll = long long;
+using ld = long double;
+
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+int main() {
+    fastio
+    int x = 0, y = 0;
+
+    int t = 0;
+    cin >> t;
+    
+    while (t--) {
+        cin >> x >> y;
+        if(y > x  || y == x){cout << "NO" << endl;}else{
+        if((y + x) % 3 == 0 && (y + x) > 0){
+            cout << "YES" << endl;
         }else{
-            cout << "NO";
+            cout << "NO" << endl;
         }
-        t--;
+
     }
+}
+
+    return 0;
 }
