435faecdb50117c62c22bdd45e8c86be97a1a600
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-28 21:10:37 -0500

    update problem heapify

--- Codeforces/Problemset´s/B. Heapify 1.cpp
+++ Codeforces/Problemset´s/B. Heapify 1.cpp
@@ -1,1 +1,21 @@
-it a
+//https://codeforces.com/problemset/problem/2195/B
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int t = 0, n = 0;
+    vector<int> a;
+    cin >> t;
+    while(t){
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        
+        t--;
+    }
+    return 0;
+}
