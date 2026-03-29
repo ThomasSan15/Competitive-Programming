74c4ddcc20de40a170ec9c33ef7fa4eef6840e66
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-28 21:21:26 -0500

    B.Heapify 1 solver

--- Codeforces/Problemset´s/B. Heapify 1.cpp
+++ Codeforces/Problemset´s/B. Heapify 1.cpp
@@ -6,15 +6,35 @@
 using namespace std;
 
 int main(){
-    int t = 0, n = 0;
-    vector<int> a;
+    int t = 0, n = 0, b = 0;
     cin >> t;
     while(t){
         cin >> n;
-        for(int i = 0; i < n; i++){
+        vector<int> a(n);
+        for(int i = 1; i <= n; i++){
             cin >> a[i];
         }
-        
+        for(int i = 1; i <= n; i++){
+            cout << a[i] << " ";
+        }
+        for(int i = 1; i <= n/2; i++){
+            if(a.at(i) > a.at(2*i)){
+                swap(a.at(i), a.at(2*i));
+            }
+        }
+        for(int i = 1; i < n - 1; i++){
+            if(a.at(i) > a.at(i + 1)){
+                cout << "NO" << endl;
+                b = 1;
+                break;
+            }
+        }
+        if(b != 1){
+            cout << "YES";
+        }
+        b = 0;
+        a.clear();
+        a.shrink_to_fit();
         t--;
     }
     return 0;
