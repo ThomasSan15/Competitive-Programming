e5a221a7da13d7d9b38a09af6b3e28e3506bd079
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-03-31 18:54:35 -0500

    add solver A.Lawn Mower

--- Codeforces/Problemset´s/A. Lawn Mower.cpp
+++ Codeforces/Problemset´s/A. Lawn Mower.cpp
@@ -1,1 +1,28 @@
 //https://codeforces.com/problemset/problem/2194/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using ll = long long;
+
+void solve(){
+    ll n ,w;
+    cin >> n >> w;
+    cout << n - (n/w) << endl;
+    
+}
+
+int main(){
+    fastio
+    int t = 0;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
