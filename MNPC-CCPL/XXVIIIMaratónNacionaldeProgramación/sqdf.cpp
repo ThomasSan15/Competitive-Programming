ff022a0e7030531c12092446802f56b4242a2e60
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-04-03 21:07:33 -0500

    add solver sqdf

---
+++ MNPC-CCPL/XXVIIIMaratónNacionaldeProgramación/sqdf.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <complex>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <array>
+#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+    int x = 1;
+    int y = 0; 
+    for( x = 1; x <= n; x++){
+    for(y = 0; y <= n; y++){
+        if((pow(x,2) - pow(y,2)) == n){
+            cout << "Y" << endl;
+            return void();
+            }
+        }
+  
+    }
+    cout << "N" << endl;
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
