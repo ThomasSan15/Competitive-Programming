52491de81c0e490425c80f3b69f1054f5e663222
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-04-07 21:01:59 -0500

    add solver weird algorithm CSES

---
+++ CSES/Introductory_Problems/Weird Algorithm.cpp
@@ -0,0 +1,62 @@
+// https://cses.fi/problemset/task/1068
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <complex>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <array>
+#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    
+}
+
+int main() {
+    fastio
+
+    ll t = 1;
+    cin >> t;
+    while (true) {
+
+        cout << t << " ";
+        if(t == 1)break;
+        if(t % 2 == 0){
+            t /= 2;
+        }else{
+            t = (t * 3) + 1;
+        }
+    }
+    cout <<  endl;
+
+    return 0;
+}
