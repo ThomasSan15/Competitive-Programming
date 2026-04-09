13b0c75fda375e6b9aaaed0e401ea972b49d3103
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-04-08 20:35:02 -0500

    add solver of operations with inversions

---
+++ Codeforces/Problemset´s/Operations with Inversions.cpp
@@ -0,0 +1,71 @@
+//https://codeforces.com/problemset/problem/2176/A
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <complex>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <array>
+#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+    vv a(n);
+    int count = 0;
+    int b = 1;
+    inp(a);
+    while(b){
+        b = 0;
+    for(ll j = n - 1, i = n - 2; i >= 0; i--, j--){
+       
+        if(a.at(j) < a.at(i)){
+            b = 1;
+            a.erase(a.begin() + j);
+            count++;
+            n--;
+        }
+    }
+}   
+    cout << count << endl;
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
