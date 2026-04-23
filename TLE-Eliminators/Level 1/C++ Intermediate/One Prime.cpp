8904b5a03bfd0dc5e461b7d17be690d1704ded4a
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-04-22 22:26:40 -0500

    add one prime solver

---
+++ TLE-Eliminators/Level 1/C++ Intermediate/One Prime.cpp
@@ -0,0 +1,66 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/H
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <complex>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <array>
+#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int x; cin >> x;
+    
+    if(x == 1) {
+        cout << "YES" << endl;
+    }else{
+      for(int i = 1; i <= x; i++ ){
+        if(x % i == 0 && i != 1 && i != x){
+            cout << "NO" << endl;
+            return void();
+        }
+      }  
+}
+cout << "YES" << endl;
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
