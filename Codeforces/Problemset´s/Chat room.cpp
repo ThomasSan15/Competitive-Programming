f174bb07b2cd922103509818e89411d6b5b4be2f
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-14 23:03:35 -0500

    add Chat Room solver

---
+++ Codeforces/Problemset´s/Chat room.cpp
@@ -0,0 +1,73 @@
+//https://codeforces.com/problemset/problem/58/A
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    string s;
+    cin >> s;
+    int count = 0;
+    for(char c: s){
+        if(count <= 4){
+        if(c == 'h' && count == 0)count++;
+        if(c == 'e' && count == 1)count++;
+        if(c == 'l' && count == 2 || c == 'l' && count == 3)count++;
+        if(c == 'o' && count == 4)count++;
+        
+        }else{
+            break;
+        }
+    }
+    if(count == 5){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
