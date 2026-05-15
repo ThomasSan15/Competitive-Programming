08244708a916cc2d832b6a9e74a883ea2476c6a2
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-14 22:40:47 -0500

    add Young Physicist solver

---
+++ Codeforces/Problemset´s/Young Physicist.cpp
@@ -0,0 +1,67 @@
+//https://codeforces.com/problemset/problem/69/A
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+    int x= 0,y = 0,z= 0, xi=0, yi=0,zi=0;
+    while(n--){
+        cin >> x >> y >> z;
+        xi += x;
+        yi += y;
+        zi += z;
+    }
+    if(xi == 0 && yi == 0 && zi == 0 ){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
