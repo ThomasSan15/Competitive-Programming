bdf37615fa9b1829cffb6333132667b60f031405
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-16 20:53:35 -0500

    add dragons solver

---
+++ Codeforces/Problemset´s/Dragons.cpp
@@ -0,0 +1,82 @@
+//https://codeforces.com/problemset/problem/230/A
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int s, n, xi = 0, yi = 0; cin >> s >> n;
+    vv ds;
+    vv point;
+    int count = n;
+    while(n--){
+        cin >> xi >> yi;
+        ds.push_back(xi);
+        point.push_back(yi);
+    }
+   
+    for(int i = 0; i < ds.size(); i++){
+        if(ds[i] != -2){
+            if(s > ds[i]){
+                s += point[i];
+                ds[i] = -2;
+                point[i] = -2;
+                count--;
+                i = -1;
+               
+            }
+        }
+         
+    }
+    if(count == 0){
+    cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
