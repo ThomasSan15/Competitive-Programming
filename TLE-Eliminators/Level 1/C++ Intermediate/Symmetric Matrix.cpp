ad963fde13967cf75f11011f2149702443b4213f
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-17 14:31:15 -0500

    add Symmetric Matrix solver

---
+++ TLE-Eliminators/Level 1/C++ Intermediate/Symmetric Matrix.cpp
@@ -0,0 +1,74 @@
+//https://codeforces.com/problemset/problem/1426/B
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n, m; cin >> n >> m;
+    int tl, tr, bl, br;
+    int b2 = 0;
+    int auxn = n;
+    while(n--){
+        cin >> tl >> tr ;
+        cin >> bl >> br ;
+        if(tr == bl) b2++;
+    }   
+  
+    if(m % 2 != 0){
+        cout <<  "NO" << endl;
+    }else{
+        if(b2 > 0){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+        
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
