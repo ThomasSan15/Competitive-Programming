579d565d40356266741102086a90b2cbe0dc39ef
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-21 22:47:07 -0500

    add peya and strings solver

---
+++ Codeforces/Problemset´s/Petya and Strings.cpp
@@ -0,0 +1,68 @@
+//https://codeforces.com/problemset/problem/112/A
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    string s1 , s2;
+    cin >> s1 >> s2;
+
+    transform(s1.begin(), s1.end(), s1.begin(), 
+                   [](unsigned char c){ return std::tolower(c); });
+    transform(s2.begin(), s2.end(), s2.begin(), 
+                   [](unsigned char c){ return std::tolower(c); });
+    if(s1 > s2){
+        cout << 1 << endl;
+    }else if(s1 == s2){
+        cout << 0 << endl;
+    }else{
+        cout << -1 << endl;
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
