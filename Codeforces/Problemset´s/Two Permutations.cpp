f9bbb3a4e4840ba5746fba938e357aa9202c4507
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-28 12:25:43 -0500

    add Two Permutations Solver

---
+++ Codeforces/Problemset´s/Two Permutations.cpp
@@ -0,0 +1,60 @@
+//https://codeforces.com/problemset/problem/1761/A
+ 
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+ 
+using namespace std;
+ 
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+ 
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+ 
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+ 
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+ 
+ 
+void solve() {
+    int n, a, b; cin >> n >> a >> b;
+    if((n== 1 || (n - (a+b) >= 2)) || (n == a  && n == b)){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
+ 
+int main() {
+    fastio
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
