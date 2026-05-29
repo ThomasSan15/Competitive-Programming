d519da6230975be98a573b3d52ff7f7dc7d8b3f9
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-05-28 21:34:59 -0500

    add MEXor Mixup solver

---
+++ Codeforces/Problemset´s/MEXor Mixup.cpp
@@ -0,0 +1,83 @@
+//https://codeforces.com/problemset/problem/1567/B
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+int computeXOR(int n)
+{
+  
+  // If n is a multiple of 4
+  if (n % 4 == 0)
+    return n;
+
+  // If n%4 gives remainder 1
+  if (n % 4 == 1)
+    return 1;
+
+  // If n%4 gives remainder 2
+  if (n % 4 == 2)
+    return n + 1;
+
+  // If n%4 gives remainder 3
+  return 0;
+}
+
+void solve() {
+    int a, b; cin >> a >> b;
+    ll xr = computeXOR(a-1);
+    
+    
+    if(xr == b){
+        cout << a << endl;
+    }else if(xr != b && (xr ^ b) != a){
+        cout << a + 1 << endl;
+    }else{
+        cout << a + 2 << endl;
+    }
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
