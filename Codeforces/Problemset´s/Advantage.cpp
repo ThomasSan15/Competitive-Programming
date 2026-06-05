fb83df245126d563624ca75f425becf66b54a2a1
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-05 18:13:04 -0500

    add Advantage solver

---
+++ Codeforces/Problemset´s/Advantage.cpp
@@ -0,0 +1,73 @@
+//https://codeforces.com/problemset/problem/1760/C
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+    vv a(n);
+    inp(a);
+    vv cop;
+    cop = a;
+    sort(ALL(cop));
+     int valmax = cop[n-1];
+     int valp = cop[n-2];
+     
+   
+
+    for(int i = 0; i < n; i++){
+        if(a[i] != valmax){
+            cout << a[i] - valmax  << " " ;
+        }else{
+            cout << valmax - valp << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
