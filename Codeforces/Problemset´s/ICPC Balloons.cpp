296f8e71d73f411b067f1da9b3e07050cb005f11
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-05 12:26:48 -0500

    add ICPC Balloons solver

---
+++ Codeforces/Problemset´s/ICPC Balloons.cpp
@@ -0,0 +1,71 @@
+//https://codeforces.com/problemset/problem/1703/B
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; 
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    set<char> st ;
+    
+    for(int i = 0; i < n; i++){
+        if(st.contains(s[i])){
+            ans++;
+        } 
+        else {
+            st.insert(s[i]);
+            ans+=2;
+    }
+}
+ cout << ans << endl;
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
