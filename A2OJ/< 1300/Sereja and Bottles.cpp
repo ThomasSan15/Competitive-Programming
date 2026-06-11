346849eefc572000d34b906ca3ca91e235af6e2f
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-11 18:20:19 -0500

    add Sereja and Bottles

---
+++ A2OJ/< 1300/Sereja and Bottles.cpp
@@ -0,0 +1,72 @@
+//https://codeforces.com/problemset/problem/315/A
+
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <algorithm>
+//#include <complex>
+//#include <map>
+//#include <set>
+//#include <string>
+//#include <vector>
+//#include <numeric>
+//#include <array>
+//#include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+
+//const ll INF = 1e18;
+//const int MOD = 1e9 + 7;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+    vv a(n), b(n);
+    int ans = n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i] >> b[i];
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j != i){
+                if(a[i] == b[j]){
+                    ans--;
+                    break;
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
