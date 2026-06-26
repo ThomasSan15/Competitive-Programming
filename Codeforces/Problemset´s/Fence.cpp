56b2ffc4898fb1fa967a15d93c0ac0245f73afde
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-26 17:28:08 -0500

    add Fence solver

---
+++ Codeforces/Problemset´s/Fence.cpp
@@ -0,0 +1,85 @@
+//https://codeforces.com/problemset/problem/363/B
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int n, k; cin >> n >> k;
+    vv a(n);
+    inp(a);
+
+    vv prefix(n+1, 0);
+    
+    for(int i = 1; i <= n; i++){
+        prefix[i] = prefix[i-1] + a[i-1];
+    }
+
+    int curr = INT_MAX;
+    int ans = 0;
+    for(int i = k, j = 0; i <= n; i++, j++){
+       
+        if(curr > prefix[i] - prefix[j]){
+            curr = prefix[i] - prefix[j];
+            ans = j + 1;
+        }
+    }
+    cout << ans << endl;
+
+    
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
