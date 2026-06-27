281498a6dca483badb20c0378f339549d76863b0
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-27 12:51:05 -0500

    add First Bad Version solver

---
+++ LeetCode/Easy/First Bad Version.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/first-bad-version/
+
+// The API isBadVersion is defined for you.
+ bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        int mid = 0;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+             if(isBadVersion(mid)){
+                high = mid - 1;
+             }else{
+                low = mid + 1;
+             }
+        }
+   
+        return low;
+    }
+};
+
+//Easy binary Search problem 
