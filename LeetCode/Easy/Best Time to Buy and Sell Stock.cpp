6dcb781702872219463ada803765949a7651d161
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-29 11:22:06 -0500

    update Best Time to Buy and Sell Stock solver

--- LeetCode/Easy/Best Time to Buy and Sell Stock.cpp
+++ LeetCode/Easy/Best Time to Buy and Sell Stock.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int ans = 0;
-        vector<int> answ(prices.size());
         int minimun = 1e8;
         for(int i = 0; i < prices.size(); i++){
             if(minimun > prices[i]){
