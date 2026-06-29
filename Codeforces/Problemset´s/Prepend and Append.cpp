ca5fe884a85848698d69f65cfeeb6fda5b31d05f
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-28 20:35:24 -0500

    add prepend and Append solver

---
+++ Codeforces/Problemset´s/Prepend and Append.cpp
@@ -0,0 +1,76 @@
+//https://codeforces.com/problemset/problem/1791/C
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    
+    ll ans = s.size();
+
+    for(int i =0, j = n -1; i <= j; i++ , j--){
+        if((s[i] == '1' && s[j] == '0' ) || (s[i] == '0' && s[j] == '1')){
+            ans -= 2;
+        }else{
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
