19a9c357f56f1ba0a6c12a683f76156b20d92ed3
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-06-29 20:03:44 -0500

    add Building Permutation solver

---
+++ A2OJ/< 1300/Building Permutation.cpp
@@ -0,0 +1,73 @@
+//https://codeforces.com/problemset/problem/285/C
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int n; cin >> n;
+    vv a(n);
+    inp(a);
+    sort(ALL(a));
+
+    ll ans = 0;
+    for(int i = 0, num = 1; i < n; i++, num++){
+        ans += abs(num - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+    
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
