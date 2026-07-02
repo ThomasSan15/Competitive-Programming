87de3c2fbdb09e38df6ee5773a08ab11714b2e14
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-07-01 23:07:52 -0500

    add Ultra Fast Mathematician solver

---
+++ Codeforces/Problemset´s/Ultra-Fast Mathematician.cpp
@@ -0,0 +1,71 @@
+//https://codeforces.com/problemset/problem/61/A
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    string s, s2;
+    cin >> s >> s2;
+
+    fore(i,0,s.size()){
+        
+        cout << ((s[i] - '0') ^ (s2[i] - '0'));
+    }
+    cout << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+    
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
