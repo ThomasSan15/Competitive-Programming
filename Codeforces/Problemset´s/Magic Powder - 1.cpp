05e9521756f8bbbdc4997d037e0104ed1f6a70d5
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-07-03 11:26:10 -0500

    add Magic Powder solver

---
+++ Codeforces/Problemset´s/Magic Powder - 1.cpp
@@ -0,0 +1,95 @@
+//https://codeforces.com/problemset/problem/670/D1
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+bool checker(int n, int k, vv &needed, vv &ingredients){
+
+    for(int i = 0;i < needed.size(); i++){
+        if(needed[i] * n >= ingredients[i]){
+            if(needed[i] * n <= ingredients[i] + k){
+                k -= needed[i] * n - ingredients[i];
+            }else{
+                return 0;
+            }
+        }
+    }
+    return 1;
+
+}
+
+void solve(){
+    int n, k; cin >> n >> k;
+    vv a(n);
+    vv b(n);
+    inp(a);
+    inp(b);
+
+    int low = 1, high = 1000 + k;
+    int mid = 0;
+
+    while(low <= high){
+        mid = low + (high - low) / 2;
+
+        if(checker(mid,k,a,b)){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    cout << high << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
