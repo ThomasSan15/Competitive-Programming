cd89fbcc4c9072449d5bd2d235cabfa0dc4f27b0
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-07-08 21:25:18 -0500

    add valid parentheses solver

---
+++ LeetCode/Easy/Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++){
+            if(!st.empty() && ((s[i] == ')'&& st.top() == '(') || (s[i] == '}' && st.top() == '{') ||
+             (s[i] == ']' && st.top() == '['))) st.pop();
+
+            else if(s[i] == '(' || s[i] == '{' || s[i] == '[')st.push(s[i]);
+
+           else return false;
+        }
+
+        return st.empty()? true : false;
+    }
+};
