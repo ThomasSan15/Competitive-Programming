b69335b60a2f66f6f1e1bfaa1964735d553701b4
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-07-13 15:47:55 -0500

    add Find the Difference solver

---
+++ LeetCode/Easy/Find the Difference.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/find-the-difference/description/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+            for(int i = 0; i < t.size(); i++){
+            if(i == s.size()) return t[i];
+            if(s[i] != t[i]) return t[i];
+        }
+        return ' ';
+    }
+};
