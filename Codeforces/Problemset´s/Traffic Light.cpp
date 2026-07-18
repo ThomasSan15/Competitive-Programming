de17f39068d2dd2188bae60365309f227379c960
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-07-17 19:25:52 -0500

    add Traffic Light solver

---
+++ Codeforces/Problemset´s/Traffic Light.cpp
@@ -0,0 +1,96 @@
+//https://codeforces.com/problemset/problem/1744/C
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int n; 
+    char c;
+    string s;
+    cin >> n >> c;
+    cin >> s;
+    int cycle =0;
+    int ans = 0;
+    int aux = 0;
+    
+    for(int i = 0, j = 0; i < n; i++){
+
+        if(s[i] == 'g'){
+            cycle = 0;
+            continue;
+        }
+
+        if(s[i]  == c && cycle == 0){
+            j = i;
+            while(s[j] != 'g'){
+                aux++;
+                j++;
+                if(j == n){
+                    j = 0;
+                }
+            }
+            cycle = 1;
+            ans = max(ans,aux);
+            aux = 0;
+        }
+
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
