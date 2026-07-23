aa03e8012e69fde349fc17cdd464cd94bee48143
Thomas Beltran <thomas.beltran@utp.edu.co>
2026-07-23 18:39:35 -0500

    Add Single Number solution in C++
    Implement solution for finding single number in an array.

---
+++ LeetCode/Easy/Single Number.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/single-number/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.size();i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
