2b00494276d24aa893a00969fb2ff6fec1b712dc
Thomas Beltran <thomas.beltran@utp.edu.co>
2026-07-29 15:03:51 -0500

    Add Maximum Multiple Sum problem solution

---
+++ Codeforces/Problemset´s/Maximum Multiple Sum.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/1985/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t; cin >> t;
+  while(t--){
+      int n;
+      cin >> n;
+     cout << (n == 3? 3: 2) << endl;
+  
+  }
+  
+
+    return 0;
+}
