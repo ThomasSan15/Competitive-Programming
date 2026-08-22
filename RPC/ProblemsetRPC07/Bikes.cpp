bc114e2a8de044d9027547848fc7e6fd36791e0c
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-08-22 17:55:52 -0500

    add ProblemsetRPC07

---
+++ RPC/ProblemsetRPC07/Bikes.cpp
@@ -0,0 +1,87 @@
+//
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int n; cin >> n;
+    vector<double> points;
+    int x1, x2, y1, y2;
+    double y = 0;
+    while(n--){
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        double t = -x1 / (double)(x2-x1);
+
+        if(0 <= t && t <= 1){
+         y = y1 + (y2-y1) * t;
+        }
+
+        if(y > 0){
+            points.pb(y);
+        }
+    }
+
+    if(points.empty()){
+        cout << "-1.0" << endl;
+    }else{
+        cout << *min_element(ALL(points)) << endl;
+    }
+}
+
+
+int main() {
+    fastio
+    
+    int t = 1;
+   
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
