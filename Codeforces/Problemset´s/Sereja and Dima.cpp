0b2d4a09fddf2a72d1105437476ef5c7d51bbf8d
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-08-31 23:31:16 -0500

    add Sereja and Dima solver

---
+++ Codeforces/Problemset´s/Sereja and Dima.cpp
@@ -0,0 +1,90 @@
+//https://codeforces.com/problemset/problem/381/A
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <algorithm>
+// #include <complex>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <array>
+// #include <cassert>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i = a, jet = b; i < jet; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) (int)(x).size()
+#define imp(v) {for(auto i : v) cout << i << " "; cout << "\n";}
+#define inp(v) {for(auto &i : v) cin >> i;}
+
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vv;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vvs;
+typedef vector<set<ll>> vst;
+
+
+// const ll INF = 1e18;
+// const int MOD = 1e9 + 7;
+
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+
+
+void solve(){
+    int sereja = 0; int dima = 0;
+    int n; cin >> n;
+    vv a(n);
+    inp(a);
+    int f = 1;
+    for(int i = 0, j = n - 1; i <= j; ){
+        if(f){
+            if(a[i] >= a[j]){
+                sereja += a[i];
+                i++;
+            }else{
+                sereja += a[j];
+                j--;
+            }
+            f = 0;
+        }else{
+             if(a[i] >= a[j]){
+                dima += a[i];
+                i++;
+            }else{
+                dima += a[j];
+                j--;
+            }
+            f = 1;
+        }
+    }
+    cout << sereja << " " << dima << endl;
+}
+
+int main() {
+    fastio
+    
+    int t = 1;
+ 
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
