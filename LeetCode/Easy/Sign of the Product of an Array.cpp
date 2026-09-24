50a207c8c38e6697074fe9c2e5d98db0d8313853
Thomas Santiago <thomas.beltran@utp.edu.co>
2026-09-24 10:59:33 -0500

    add Sign of the Product of an Array solver

---
+++ LeetCode/Easy/Sign of the Product of an Array.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/sign-of-the-product-of-an-array/description/
+
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0)return 0;
+            if(nums[i] < 0)neg++;
+        }
+        return (neg % 2 == 0 ? 1 : -1);
+    }
+};
